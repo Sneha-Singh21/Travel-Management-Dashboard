fd420894eb1bbbe6d8aa73bc8b55c45781e33551
Sneha <indsneha2002@gmail.com>
2025-07-18 15:50:51 +0530

    changes made

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminLayout = () => {
+  return (
+    <div className='admin-layout'>
+        <h1>Welcome to the admin page..</h1>
+    </div>
+  )
+}
+
+export default AdminLayout;
